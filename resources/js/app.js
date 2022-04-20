19a410be2cb5b7eede3004e7b10f861c47fc9ca1
raselinfo <raselinfo52@gmail.com>
2022-04-20 14:30:45 -0700

    Noty install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,20 @@
 import axios from "axios"
+import Noty from "noty"
 const btns = document.getElementsByClassName("btn");
 const cartQty = document.getElementById("cartQty")
 
 
 async function updateCart(pizza) {
     let response = await axios.post('/updateCart', pizza)
-    console.log(response)
     cartQty.innerHTML = response.data.totalQnt
+
+    new Noty({
+        text: 'Some notification text',
+        timeout:1000,
+        sounds:{
+            volume:0.5
+        }
+    }).show();
 }
 
 ;[...btns].forEach(btn => {
